0d5500835323f871c62fbd4f9d28b7b0d5146895
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-11-04 13:30:07 +0100

    improve keyed-list performance

--- samples/index.tsx
+++ samples/index.tsx
@@ -7,15 +7,20 @@ import { expr } from 'callbag-expr';
 
 const renderer = makeRenderer();
 
-const count = 1000;
-const list = state<number[]>([]);
+// const count = 1000;
+// const list = state<number[]>([]);
 
-renderer.render(<>
-  <button onclick={() => {
-    const l = [];
-    const s = list.get().length;
-    for (let i = 0; i < count; i++) { l.push(i + s); }
-    list.set(list.get().concat(l));
-  }}>ADD</button>
-  <List of={list} each={n => <div>{n}</div>} key={i => i}/>
-</>).on(document.body);
+// renderer.render(<>
+//   <button onclick={() => {
+//     const l = [];
+//     const s = list.get().length;
+//     for (let i = 0; i < count; i++) { l.push(i + s); }
+//     list.set(list.get().concat(l));
+//   }}>ADD</button>
+//   <List of={list} each={n => <div>{n}</div>} key={i => i}/>
+// </>).on(document.body);
+
+const l = state(['A', 'B', 'C', 'D']);
+renderer.render(<List of={l} each={x => <>{x}</>} key={x => x}/>).on(document.body);
+
+l.set(['D', 'A', 'E', 'F', 'C', 'G', 'H']);
--- src/components/keyed-list.tsx
+++ src/components/keyed-list.tsx
@@ -25,6 +25,45 @@ function isWithKeys<T>(props: KeyedListProps<T>): props is KeyedListPropsWithKey
   return (props as any).key && typeof (props as any).key === 'function';
 }
 
+
+type Shift = [from: number, to: number];
+function tr(i: number, shifts: Shift[]) {
+  let res = i;
+  for (let _i = 0, shift = shifts[_i]; _i < shifts.length; shift = shifts[++_i]) {
+    res = res >= shift[0] ? res + shift[1] : res;
+  }
+
+  return res;
+}
+
+function del(i: number, markers: Node[], startMark: Node, renderer: LiveDOMRenderer, shifts: Shift[]) {
+  const index = tr(i, shifts);
+  const start = markers[index - 1] || startMark;
+  const end = markers[index];
+  const nodes = scanRemove(start, end, { includeEnd: true, remove: n => renderer.remove(n) });
+  markers.splice(index, 1);
+  shifts.push([index, -1]);
+
+  return nodes;
+}
+
+function add(
+  i: number, nodes: Node[], hasMarker: boolean,
+  markers: Node[], startMark: Node, renderer: LiveDOMRenderer, shifts: Shift[]
+) {
+  const index = i;
+  const start = markers[index - 1] || startMark;
+  const marker = hasMarker ? nodes[nodes.length - 1] : renderer.leaf();
+  const frag = <></>;
+  for (let _i = 0, node = nodes[_i]; _i < nodes.length; node = nodes[++_i]) {
+    renderer.render(node).on(frag);
+  }
+  if (!hasMarker) { renderer.render(marker).on(frag); }
+  renderer.render(frag).after(start);
+  markers.splice(index, 0, marker);
+  shifts.push([index, +1]);
+}
+
 export function KeyedList<T>(
   this: TrackerComponentThis & LiveDOMComponentThis,
   props: KeyedListProps<T>,
@@ -50,13 +89,17 @@ export function KeyedList<T>(
 
   this.onBind(() => {
     const frag = <></>;
-    (src.get() || []).forEach((entry: any) => {
+    for (
+      let i = 0, _src = src.get() || [], entry = _src[i];
+      i < _src.length;
+      entry = _src[++i]
+    ) {
       const key = src.keyfunc(entry);
       const marker = renderer.leaf();
       markers.push(marker);
       renderer.render(props.each(src.key(key), src.index(key))).on(frag);
       renderer.render(marker).on(frag);
-    });
+    }
     renderer.render(frag).after(startMark);
   });
 
@@ -65,63 +108,47 @@ export function KeyedList<T>(
       return;
     }
 
-    const shifts: [index: number, shift: number][] = [];
-    const tr = (i: number) => shifts.reduce((_i, shift) => _i >= shift[0] ? _i + shift[1] : _i, i);
-
-    const del = (i: number) => {
-      const index = tr(i);
-      const start = markers[index - 1] || startMark;
-      const end = markers[index];
-      const nodes = scanRemove(start, end, { includeEnd: true, remove: n => renderer.remove(n) });
-      markers.splice(index, 1);
-      shifts.push([index, -1]);
-
-      return nodes;
-    };
-
-    const add = (i: number, nodes: Node[], hasMarker = false) => {
-      const index = i;
-      const start = markers[index - 1] || startMark;
-      const marker = hasMarker ? nodes[nodes.length - 1] : renderer.leaf();
-      const frag = <></>;
-      nodes.forEach(node => renderer.render(node).on(frag));
-      if (!hasMarker) { renderer.render(marker).on(frag); }
-      renderer.render(frag).after(start);
-      markers.splice(index, 0, marker);
-      shifts.push([index, +1]);
-    };
+    const shifts: Shift[] = [];
 
     const appends: Addition<T>[] = [];
-    changes.deletions.forEach(deletion => del(deletion.index));
-    changes.additions.forEach(addition => {
-      if (tr(addition.index) > markers.length) {
-        appends.push(addition);
+    for (let i = 0, d = changes.deletions[i]; i < changes.deletions.length; d = changes.deletions[++i]) {
+      del(d.index, markers, startMark, renderer, shifts);
+    }
+
+    for (let i = 0, a = changes.additions[i]; i < changes.additions.length; a = changes.additions[++i]) {
+      if (tr(a.index, shifts) > markers.length) {
+        appends.push(a);
       } else {
-        const key = src.keyfunc(addition.item);
-        add(addition.index, [props.each(src.key(key), src.index(key))]);
+        const key = src.keyfunc(a.item);
+        add(a.index, [props.each(src.key(key), src.index(key))], false, markers, startMark, renderer, shifts);
       }
-    });
+    }
 
-    changes.moves
-    .filter(move => tr(move.oldIndex) !== move.newIndex)
-    .sort((a, b) =>  Math.abs(tr(b.oldIndex) - b.newIndex) - Math.abs(tr(a.oldIndex) - a.newIndex))
-    .forEach(move => {
-      if (tr(move.oldIndex) === move.newIndex) { return; }
+    let moves = [];
+    for (let i = 0, m = changes.moves[i]; i < changes.moves.length; m = changes.moves[++i]) {
+      if (tr(m.oldIndex, shifts) !== m.newIndex) {
+        moves.push(m);
+      }
+    }
+    moves = moves.sort((a, b) =>
+      Math.abs(tr(b.oldIndex, shifts) - b.newIndex) - Math.abs(tr(a.oldIndex, shifts) - a.newIndex));
+    for (let i = 0, move = moves[i]; i < moves.length; move = moves[++i]) {
+      if (tr(move.oldIndex, shifts) === move.newIndex) { continue; }
 
-      const nodes = del(move.oldIndex);
-      add(move.newIndex, nodes, true);
-    });
+      const nodes = del(move.oldIndex, markers, startMark, renderer, shifts);
+      add(move.newIndex, nodes, true, markers, startMark, renderer, shifts);
+    }
 
     if (appends.length > 0) {
       const frag = <></>;
       const start = markers[markers.length - 1];
-      appends.forEach(append => {
+      for (let i = 0, a = appends[i]; i < appends.length; a = appends[++i]) {
         const marker = renderer.leaf();
         markers.push(marker);
-        const key = src.keyfunc(append.item);
+        const key = src.keyfunc(a.item);
         renderer.render(props.each(src.key(key), src.index(key))).on(frag);
         renderer.render(marker).on(frag);
-      });
+      }
 
       renderer.render(frag).after(start);
     }
