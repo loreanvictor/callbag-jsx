384acd73c4113553956dfc9561c3846f40179e59
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-11-14 12:07:13 +0100

    fix table responsiveness

--- .codedoc/components/table.tsx
+++ .codedoc/components/table.tsx
@@ -3,7 +3,7 @@ import { RendererLike } from '@connectv/html';
 export function Table({header, body}: {header: any, body: any}, renderer: RendererLike<any, any>, content: any) {
 
   return <div style='overflow-x: auto'>
-    <table style='width: 100%;'>
+    <table style='min-width: 100%;'>
       <thead>{header}</thead>
       <tbody>{body}</tbody>
     </table>
