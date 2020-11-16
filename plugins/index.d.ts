add90cd1c67e06371ae4b462194d2c19328eb302
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-11-16 14:05:57 +0100

    export plugins

---
+++ plugins/index.d.ts
@@ -0,0 +1,10 @@
+export {
+  CallbagAppendPlugin,
+  CallbagClassPlugin,
+  CallbagContentPlugin,
+  CallbagEventHandlerPlugin,
+  CallbagInputStatePlugin,
+  CallbagPropPlugin,
+  CallbagStylePlugin,
+  CallbagTrackPlugin,
+} from '../dist/es6/plugins';
